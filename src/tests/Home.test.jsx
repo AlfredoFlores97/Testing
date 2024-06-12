28eb0f6d2c4f9b74287664c4a0a7ffc2489174ef
Alfredo Flores <alfredoflores.cv@gmail.com>
2024-06-12 18:12:28 +0200

    prueba commit correct test 2

--- src/tests/Home.test.jsx
+++ src/tests/Home.test.jsx
@@ -90,7 +90,7 @@ describe('Home component', () => {
 
         // Verificar que los valores de los inputs se actualizan
         expect(emailInput.value).toBe(username);
-        expect(passwordInput.value).toBe('password');
+        expect(passwordInput.value).toBe(password);
     });
 
     test('handles form submission', () => {
